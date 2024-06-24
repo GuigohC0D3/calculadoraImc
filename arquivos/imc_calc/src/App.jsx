d74132ebae23475791e1cf59380f7c9a9ca99fdc
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2024-06-24 11:47:41 -0300

    Css e jsx feitos

--- arquivos/imc_calc/src/App.jsx
+++ arquivos/imc_calc/src/App.jsx
@@ -1,4 +1,5 @@
-import ImcCalc from "./components/ImcCalc"
+import ImcCalc from "./components/ImcCalc";
+import Button from "./components/Button";
 
 import "./App.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="container">
       <ImcCalc />
+      <Button />
     </div>
   );
 }
